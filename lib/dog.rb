83742a9edd34def7bbc35a6909a83aeedc374eed
Mia <tamiapaschall@gmail.com>
2020-01-06 03:44:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,2 @@
-class Dog 
-  
-  def initialize(name, breed="Mutt")
-    @name
-    @breed
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-  
-end 
+
     
